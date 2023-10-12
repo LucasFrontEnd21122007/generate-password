4ac7677832e8557112f8b0db8830ad9bfd66f750
LUCAS MARTINEZ <132151483+LucasFrontEnd21122007@users.noreply.github.com>
2023-10-12 12:14:18 -0300

    arquivos do projeto
    arquivos do projeto

---
+++ index.js
@@ -0,0 +1,13 @@
+function generatePassword() {
+    const length = 12; // Length of the password
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+"; // Characters to use
+    let password = "";
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset.charAt(randomIndex);
+    }
+
+    const passwordField = document.getElementById("password");
+    passwordField.value = password;
+}
